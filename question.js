a9307780b92783ccb1ac25cf013f4e427d872187
Divyamshini-Rebba <157277776+Divyamshini-Rebba@users.noreply.github.com>
2024-11-06 21:14:37 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,58 @@
+let questions=[
+    {
+        numb:1,
+        question:"Who is the dance God?",
+        answer:"B.Nataraja",
+        options:[
+            "A.Lord Shiva",
+            "B.Nataraja",
+            "C.Rukminidevi Arundale",
+            "D.Krishna Iyer"
+        ]
+    },
+    {
+        numb:2,
+        question:"Who invented Bharatanatyam?",
+        answer:"A.Krishna Iyer and Rukminidevi Arundale",
+        options:[
+            "A.Krishna Iyer and Rukminidevi Arundale",
+            "B.Nataraja",
+            "C.God",
+            "D.Bhava,Raga,Tala, and Natya"
+        ]
+    },
+    {
+        numb:3,
+        question:"What equals Bhartanatyam?",
+        answer:"D.Bhava , Raga , Tala , Natyam= Bharatanatyam",
+        options:[
+            "A.Tisram , Chaturaswram , Misram , ThaKita= Bharatanatyam",
+            "B.There is no such thing like that....",
+            "C.I DO NOT KNOW!",
+            "D.Bhava , Raga , Tala , Natyam= Bharatanatyam"
+        ]
+    },
+    {
+        numb:4,
+        question:"Bharathanatyam has originated from?",
+        answer:"D.Tamilnadu",
+        options:[
+            "A.Maharashtra",
+            "B.Uttar Pradesh",
+            "C.Gujarat",
+            "D.Tamilnadu"
+        ]
+    },
+    {
+        numb:5,
+        question:"What is the word for the red paint the dancers wear on the palms of their hands and on their feet?",
+        answer:"C.Alta",
+        options:[
+            "A.Henna",
+            "B.Paint",
+            "C.Alta",
+            "D.Tempera"
+
+        ]
+    }
+];
